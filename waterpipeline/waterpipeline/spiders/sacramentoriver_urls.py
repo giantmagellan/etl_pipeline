ba43bb74745d03d33817aabfd59e3584b077980b
Caleb McCurdy <105075842+CalebMcCurdy@users.noreply.github.com>
2023-02-16 16:36:21 -0500

    Create sacramentoriver_urls.py

---
+++ waterpipeline/waterpipeline/spiders/sacramentoriver_urls.py
@@ -0,0 +1,5 @@
+sr_nitrates = 'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=99133&startDT=2023-02-09T13:26:39.886-08:00&endDT=2023-02-16T13:26:39.886-08:00&siteStatus=all&format=rdb'
+
+sr_ph = 'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=00400&startDT=2023-02-09T13:25:01.674-08:00&endDT=2023-02-16T13:25:01.674-08:00&siteStatus=all&format=rdb'
+
+sr_salinity = 'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=00480&startDT=2023-02-09T13:33:20.613-08:00&endDT=2023-02-16T13:33:20.613-08:00&siteStatus=all&format=rdb'
