5a2bcdf94c6b68deef8890c2fd5bc0ac042cd1ed
Caleb McCurdy <105075842+CalebMcCurdy@users.noreply.github.com>
2023-02-16 17:09:23 -0500

    Update sacramentoriver_spider.py

--- waterpipeline/waterpipeline/spiders/sacramentoriver_spider.py
+++ waterpipeline/waterpipeline/spiders/sacramentoriver_spider.py
@@ -9,8 +9,11 @@ class WaterSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             # sr_nitrates
-            'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=99133&startDT=2023-02-09T13:26:39.886-08:00&endDT=2023-02-16T13:26:39.886-08:00&siteStatus=all&format=rdb'
-          
+            'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=99133&startDT=2023-02-09T13:26:39.886-08:00&endDT=2023-02-16T13:26:39.886-08:00&siteStatus=all&format=rdb',
+            #sr_ph
+            'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=00400&startDT=2023-02-09T13:25:01.674-08:00&endDT=2023-02-16T13:25:01.674-08:00&siteStatus=all&format=rdb',
+            #sr_salinity
+            'https://waterservices.usgs.gov/nwis/iv/?sites=11447890&parameterCd=00480&startDT=2023-02-09T13:33:20.613-08:00&endDT=2023-02-16T13:33:20.613-08:00&siteStatus=all&format=rdb',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
