b229e970c613d3c928a81f8f3a2f2d3da4f92825
Caleb McCurdy <105075842+CalebMcCurdy@users.noreply.github.com>
2023-02-21 22:09:29 -0500

    middleriver_urls.py f-string time variables

--- waterpipeline/waterpipeline/spiders/middleriver_urls.py
+++ waterpipeline/waterpipeline/spiders/middleriver_urls.py
@@ -1,5 +1,16 @@
-mr_nitrates = 'https://waterservices.usgs.gov/nwis/iv/?sites=11312676&parameterCd=99133&startDT=2023-01-29T13:06:19.614-08:00&endDT=2023-02-05T13:06:19.614-08:00&siteStatus=all&format=rdb'
 
-mr_ph = 'https://waterservices.usgs.gov/nwis/iv/?sites=11312676&parameterCd=00400&startDT=2023-01-29T13:17:45.486-08:00&endDT=2023-02-05T13:17:45.486-08:00&siteStatus=all&format=rdb'
+current_time = datetime.datetime.now().isoformat()
 
-mr_salinity = 'https://waterservices.usgs.gov/nwis/iv/?sites=11312676&parameterCd=00480&startDT=2023-01-29T13:18:33.327-08:00&endDT=2023-02-05T13:18:33.327-08:00&siteStatus=all&format=rdb'
+week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+week_ago_iso = week_ago.isoformat()
+
+#print("Current time (ISO 8601 format):", current_time)
+#print("One week ago (ISO 8601 format):", week_ago)
+#print("One week ago:", week_ago_iso)
+
+
+mr_nitrates = f'https://waterservices.usgs.gov/nwis/iv/?sites=11312676&parameterCd=99133&startDT={week_ago_iso}&endDT={current_time}&siteStatus=all&format=rdb'
+
+mr_ph = f'https://waterservices.usgs.gov/nwis/iv/?sites=11312676&parameterCd=00400&startDT={week_ago_iso}&endDT={current_time}&siteStatus=all&format=rdb'
+
+mr_salinity = f'https://waterservices.usgs.gov/nwis/iv/?sites=11312676&parameterCd=00480&startDT={week_ago_iso}&endDT={current_time}&siteStatus=all&format=rdb'
