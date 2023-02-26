c62fca11c73024a97364a715f5a066c4598aa157
Kenzie C <97000763+mc3435@users.noreply.github.com>
2023-02-26 14:38:57 -0800

    Add files via upload

---
+++ wip_airflow/include/Both_spiders.py
@@ -0,0 +1,61 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+class WaterSpider(scrapy.Spider):
+    name = "sacramentoriver"
+
+    def start_requests(self):
+        url_init = "https://waterservices.usgs.gov/nwis/iv/?sites="
+        url_end = "&siteStatus=all&format=rdb"
+        monitor_loc = "11447890"
+        param_codes = ['99133', '00400', '00480']  # nitrates, pH, salinity
+        end_date = date.today()
+        week_ago = end_date - timedelta(days=7)  # obtain data for the past 7 days
+        start_date = end_date - timedelta(days=1) # obtain data from the past day
+
+        if week_ago == end_date:
+            urls = ['{}{}&parameterCd={}&startDT={}&endDT={}{}'.format(url_init, monitor_loc, code, week_ago, end_date, url_end) for code in param_codes]
+        else:
+            urls = ['{}{}&parameterCd={}&startDT={}&endDT={}{}'.format(url_init, monitor_loc, code, start_date, end_date, url_end) for code in param_codes]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("parameterCd=")[-1].split("&")[0]
+        filename = f'waterpipeline/data_samples/sacramentoriver-{page}.csv'
+        data = response.body.decode("utf-8")  
+        csv = pd.read_csv(io.StringIO(data), sep="\t", skiprows=26)
+        csv = csv.to_csv(filename, index=False)
+        Path(csv).write_bytes(data)
+        self.log(f'Saved file {csv}')
+
+class WaterSpider(scrapy.Spider):
+    name = "ryerisland"
+
+    def start_requests(self):
+        url_init = "https://waterservices.usgs.gov/nwis/iv/?sites="
+        url_end = "&siteStatus=all&format=rdb"
+        monitor_loc = "11455385"
+        param_codes = ['99133', '00400', '00480']  # nitrates, pH, salinity
+        end_date = date.today()
+        week_ago = end_date - timedelta(days=7)  # obtain data for the past 7 days
+        start_date = end_date - timedelta(days=1) # obtain data from the past day
+
+        if week_ago == end_date:
+            urls = ['{}{}&parameterCd={}&startDT={}&endDT={}{}'.format(url_init, monitor_loc, code, week_ago, end_date, url_end) for code in param_codes]
+        else:
+            urls = ['{}{}&parameterCd={}&startDT={}&endDT={}{}'.format(url_init, monitor_loc, code, start_date, end_date, url_end) for code in param_codes]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("parameterCd=")[-1].split("&")[0]
+        filename = f'waterpipeline/data_samples/ryerisland-{page}.csv'
+        data = response.body.decode("utf-8")  
+        csv = pd.read_csv(io.StringIO(data), sep="\t", skiprows=26)
+        csv = csv.to_csv(filename, index=False)
+        Path(csv).write_bytes(data)
+        self.log(f'Saved file {csv}')
---
+++ wip_airflow/include/ryerisland_spider.py
@@ -0,0 +1,38 @@
+# USGS SITE: CACHE SLOUGH AB RYER ISLAND FERRY NR RIO VISTA CA
+# SITE NO.: 11455385
+
+from pathlib import Path
+from datetime import date, timedelta
+import pandas as pd
+import io
+
+import scrapy
+
+class WaterSpider(scrapy.Spider):
+    name = "ryerisland"
+
+    def start_requests(self):
+        url_init = "https://waterservices.usgs.gov/nwis/iv/?sites="
+        url_end = "&siteStatus=all&format=rdb"
+        monitor_loc = "11455385"
+        param_codes = ['99133', '00400', '00480']  # nitrates, pH, salinity
+        end_date = date.today()
+        past_week = end_date - timedelta(days=7)  # obtain data for the past 7 days
+        start_date = end_date - timedelta(days=1) # obtain data from the past day
+
+        if past_week == end_date:
+            urls = ['{}{}&parameterCd={}&startDT={}&endDT={}{}'.format(url_init, monitor_loc, code, past_week, end_date, url_end) for code in param_codes]
+        else:
+            urls = ['{}{}&parameterCd={}&startDT={}&endDT={}{}'.format(url_init, monitor_loc, code, start_date, end_date, url_end) for code in param_codes]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("parameterCd=")[-1].split("&")[0]
+        filename = f'waterpipeline/data_samples/ryerisland-{page}.csv'
+        data = response.body.decode("utf-8")  
+        csv = pd.read_csv(io.StringIO(data), sep="\t", skiprows=26)
+        csv = csv.to_csv(filename, index=False)
+        Path(csv).write_bytes(response.body)
+        self.log(f'Saved file {csv}')
