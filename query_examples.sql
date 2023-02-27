aa1b63a1089d24e5409c96d4a91e92b4331225c6
Caleb McCurdy <105075842+CalebMcCurdy@users.noreply.github.com>
2023-02-27 04:36:08 -0500

    Update query_examples.sql

--- query_examples.sql
+++ query_examples.sql
@@ -1,6 +1,6 @@
 use water_quality;
 
-# Datetime and nitrate level where collection @ Sacramento River results in nitrate levels less than 0.4 mg per liter
+# Nitrate levels where collection @ Sacramento River results in less than 0.4 mg per liter
 SELECT
 	collection_datetime,
     nitrate_level
@@ -14,9 +14,10 @@ SELECT
 FROM ryerisland
 WHERE pH_value >= 8;
 
-# Datetime and salinity levels @ Sacramento River during 11 o'clock hour
+# Salinity and nitrate levels @ Sacramento River during 11 o'clock hour
 SELECT
     collection_datetime,
-    salinity_level
+    salinity_level,
+    nitrate_level
 FROM sacriver
 WHERE collection_datetime LIKE '%11:%';
