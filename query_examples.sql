ab062ff953e6d6ea4e75689f2fe9fbb6e352fae7
Caleb McCurdy <105075842+CalebMcCurdy@users.noreply.github.com>
2023-02-27 04:37:10 -0500

    Update query_examples.sql

--- query_examples.sql
+++ query_examples.sql
@@ -2,14 +2,14 @@ use water_quality;
 
 # Nitrate levels where collection @ Sacramento River results in less than 0.4 mg per liter
 SELECT
-	collection_datetime,
+    collection_datetime,
     nitrate_level
 FROM sacriver
 WHERE nitrate_level < 0.4;
 
 # Datetime and pH where collection @ Ryer Island results in a pH value of at least 8
 SELECT
-	collection_datetime,
+    collection_datetime,
     pH_value
 FROM ryerisland
 WHERE pH_value >= 8;
